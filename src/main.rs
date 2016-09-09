9bd36c6e3b4af8d4e71472eea134b6d294e1e7ce
BookOwl <stanleybookowl@gmail.com>
2016-09-09 08:02:12 -0400

    Add the tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::io;
+extern crate regex;
+#[macro_use] extern crate lazy_static;
+
+use regex::Regex;
+
 fn main() {
     loop {
         println!("Enter input:");
@@ -13,10 +18,42 @@ fn main() {
 }
 
 enum Token {
-    Number (u32),
-    Operation (&str),
+    Number (i64),
+    Plus,
+    Sub,
+    Mul,
+    Div,
 }
 
-fn tokenize(&str input) -> Vec<Token> {
-    
+fn tokenize(mut input: String) -> Vec<Token> {
+    lazy_static! {
+        static ref NUMBER_RE: Regex = Regex::new(r"^-?[0-9]+").unwrap();
+    }
+    let mut res = vec![];
+    while !(input == "") {
+        input = input.trim_left().to_string();
+        input = if let Some((_, end)) = NUMBER_RE.find(&input) {
+            let (num, rest) = input.split_at_mut(end);
+            res.push(Token::Number(num.parse::<i64>().unwrap()));
+            rest.to_string()
+        } else {
+            res.push(match &input[0..1] {
+                "+" => Token::Plus,
+                "-" => Token::Sub,
+                "*" => Token::Mul,
+                "/" => Token::Div,
+                _ => panic!("Unknown char!"),
+            });
+            input[1..].to_string()
+        }
+    }
+    res
+}
+
+fn shunt(tokens: Vec<Token>) -> Vec<Token> {
+    vec![Token::Number(0)]
+}
+
+fn calculate(stack: Vec<Token>) -> i64 {
+    0
 }
